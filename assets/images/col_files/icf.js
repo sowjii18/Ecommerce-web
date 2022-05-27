32ff6475235482042691ccbd9a0de1aa0741b5b3
sowjii18 <sowjanya.actimize@gmail.com>
2022-05-27 18:36:37 +0530

    adding files

--- assets/images/col_files/icf.js
+++
@@ -1,5 +0,0 @@
-var n = document.getElementsByTagName("script")[0];
-var r = document.createElement("script");
-r.type = "text/javascript";
-r.src = "https://www.improvedcontactform.com/js/loader.js";
-n.parentNode.insertBefore(r, n);
